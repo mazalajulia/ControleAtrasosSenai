9545b6de865cfd821687e669e1b46759da8753b7
mazalajulia <julia.m.carvalho@aluno.senai.br>
2025-12-04 08:11:28 -0300

    fim

--- src/main/java/com/senai/infob/ControleAtrasos/controllers/AtrasoController.java
+++ src/main/java/com/senai/infob/ControleAtrasos/controllers/AtrasoController.java
@@ -8,12 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.senai.infob.ControleAtrasos.models.Atraso;
 import com.senai.infob.ControleAtrasos.services.AtrasoService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 
 @RestController
